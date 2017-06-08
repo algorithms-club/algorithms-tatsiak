5ab2656138ad0d003e25711bccb8c258c092fab9
tatsiak <tatsiak@MacBook-Air-mjve2.local>
2017-06-08 23:30:24 +0300

    started work on BST

--- app/binarySearchTree.js
+++ app/binarySearchTree.js
@@ -4,50 +4,19 @@
 'use strict';
 
 class Node {
-    constructor(value, previous = null) {
-        this._value = value;
-        this._previous = previous;
-        this._next = null;
-    }
-}
-class LinkedList {
-    constructor() {
-        this._start = null;
-        this._end = null;
-    }
-    pushIntoEnd(value) {
-        if (this._start === null) {
-            this._start = new Node(value);
-            this._end = this._start;
-        } else {
-            this._end._next = new Node(value, this._end);
-            this._end = this._end._next;
-        }
-    }
-    pushIntoStart(value) {
-        if (this._start === null) {
-            this._start = new Node(value);
-            this._end = this._start;
-        } else {
-            this._start._previous = new Node(value);
-            this._start._next = this._start;
-            this._start = this._start._previous;
-        }
-    }
-    pop() {
-        if (this._start === null) {
-            return;
-        }
-        let last = this._end._value;
-        if (!this._end._previous) {
-            this._start = null;
-            this._end = this._start;
-            return last;
-        }
-        this._end._previous._next = null;
-        this._end = this._end._previous;
-        return last;
+    constructor(left, key, value, right, parent) {
+        this._left = left;
+        this._right = right;
+        this._parent = parent;
+        this._key = key;
+        this._value = value
     }
 }
 
-module.exports = LinkedList;
+
+class BST {
+    addNode(node);
+    findNode(node);
+}
+
+module.exports = BST;

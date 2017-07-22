db3f8b974f6e73102d98558b88c7c4965ef767c0
tatsiak <tatsiak@gmail.com>
2017-07-23 01:11:23 +0300

    deleted comment

--- test/textJustify.spec.js
+++ test/textJustify.spec.js
@@ -6,7 +6,6 @@ describe.only('#textJustify', function () {
         it('should built strings no longer than second argument', function () {
             let str = "At ConHugeCo, we understand how to exploit vertically. Do you have a scheme to become B2C2B? Without meticulously-planned That is a remarkable achievement when you consider the current fiscal ye";
             let justify = new algolib.textJustify(str, 40);
-            // console.log(justify)
         })
     });
 });
